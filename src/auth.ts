16d6663e1ad4ca0b2bb8f213eafdd2a7c01df826
weasy1as <habdu21@student.sdu.dk>
2025-05-26 20:16:07 +0200

    added secret and id

--- src/auth.ts
+++ src/auth.ts
@@ -5,5 +5,11 @@ import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
